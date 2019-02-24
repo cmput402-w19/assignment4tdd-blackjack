46cb871314f32c0c201a75dc1a91b611dae88900
tyheise <tyler.heise@gmail.com>
2019-02-23 21:58:17 -0700

    Made getScoreTest more thorough. REFACTOR.

--- src/test/java/blackjack/CardTest.java
+++ src/test/java/blackjack/CardTest.java
@@ -6,7 +6,16 @@ import junit.framework.TestCase;
 public class CardTest extends TestCase {
 	
 	public void testGetScore(){
-		Card card = new Card("king", "s");
+		Card card = new Card("king", "spade");
+		assertEquals(card.getScore(), 10);
+		
+		card = new Card("ace", "hearts");
+		assertEquals(card.getScore(), 11);
+		
+		card = new Card("1", "clubs");
+		assertEquals(card.getScore(), 1);
+		
+		card = new Card("10", "diamonds");
 		assertEquals(card.getScore(), 10);
 	}
 
