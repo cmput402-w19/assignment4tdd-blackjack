f2cf3556974525a4deaa312fbf731e20f4b732ec
tyheise <tyler.heise@gmail.com>
2019-02-23 23:15:22 -0700

    Added skelton methods to Hand class and a test for getScore(). RED.

--- src/main/java/blackjack/Hand.java
+++ src/main/java/blackjack/Hand.java
@@ -1,5 +1,24 @@
 package blackjack;
 
-public class Hand {
+import java.util.ArrayList;
 
+public class Hand {
+	ArrayList<Card> cards;
+	
+	public Hand(ArrayList<Card> cards){
+		this.cards = cards;
+	}
+	
+	public int getScore(){
+		return 0;
+	}
+	
+	public int getCount(){
+		return 0;
+	}
+
+	public ArrayList<Card> getCards(){
+		return this.cards;
+	}
+	
 }
