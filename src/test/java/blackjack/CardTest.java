849255759243d082612997bbe9e161cda029b24c
tyheise <tyler.heise@gmail.com>
2019-02-23 19:00:59 -0700

    Added test for Card getScore() RED.

--- src/test/java/blackjack/CardTest.java
+++ src/test/java/blackjack/CardTest.java
@@ -2,6 +2,12 @@ package blackjack;
 
 import junit.framework.TestCase;
 
+//spades, hearts, clubs, diamonds
 public class CardTest extends TestCase {
+	
+	public void testGetScore(){
+		Card card = Card('k', 's');
+		assertEquals(card.getScore(), 10);
+	}
 
 }
