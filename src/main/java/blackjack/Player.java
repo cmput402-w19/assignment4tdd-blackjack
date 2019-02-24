5968bd662be74b635cc635fc6fa878011c2de39f
tyheise <tyler.heise@gmail.com>
2019-02-24 12:59:39 -0700

    Added skeleton methods for player and player test. Made player.getHand() test. RED.

--- src/main/java/blackjack/Player.java
+++ src/main/java/blackjack/Player.java
@@ -1,5 +1,44 @@
 package blackjack;
 
 public class Player {
-
+	private Hand hand;
+	private int wins;
+	private int losses;
+	private int ties;
+	
+	public Player(Hand hand){
+		
+	}
+	
+	public Hand getHand(){
+		return new Hand(null);
+	}
+	
+	public int getWins(){
+		return 0;
+	}
+	
+	public int getLosses(){
+		return 0;
+	}
+	
+	public int getTies(){
+		return 0;
+	}
+	
+	public void setHand(Hand hand){
+		
+	}
+	
+	public void setWins(int wins){
+		
+	}
+	
+	public void setLosses(int losses){
+		
+	}
+	
+	public void setTies(int ties){
+		
+	}
 }
--- src/test/java/blackjack/PlayerTest.java
+++ src/test/java/blackjack/PlayerTest.java
@@ -1,7 +1,57 @@
 package blackjack;
 
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
 
 public class PlayerTest extends TestCase {
 
+	public void testGetHand(){
+		Card card2 = new Card("king","hearts");
+		Card card3 = new Card("queen","hearts");
+		ArrayList<Card> cards2 = new ArrayList<Card>();
+		cards2.add(card2);
+		cards2.add(card3);
+		Hand hand2 = new Hand(cards2);
+		Player player = new Player(hand2);
+		
+		assertEquals(player.getHand(), hand2);
+		
+	}
+	
+	public void testGetWins(){
+		Player player = new Player(null);
+		
+	}
+	
+	public void testGetLosses(){
+		Player player = new Player(null);
+		
+	}
+	
+	public void testGetTies(){
+		Player player = new Player(null);
+		
+	}
+	
+	public void testSetHand(){
+		Player player = new Player(null);
+		
+	}
+	
+	public void testSetWins(){
+		Player player = new Player(null);
+		
+	}
+	
+	public void testSetLosses(){
+		Player player = new Player(null);
+		
+	}
+	
+	public void testSetTies(){
+		Player player = new Player(null);
+		
+	}
+	
 }
