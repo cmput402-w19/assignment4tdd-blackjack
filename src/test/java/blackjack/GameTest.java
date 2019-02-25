77e3b04acc35de0aaba3135478d3db025d36c4a4
tyheise <tyler.heise@gmail.com>
2019-02-24 17:43:47 -0700

    Added failing test and changed mvn script.

--- src/test/java/blackjack/GameTest.java
+++ src/test/java/blackjack/GameTest.java
@@ -5,6 +5,6 @@ import junit.framework.TestCase;
 public class GameTest extends TestCase {
 	public void testGiveOne(){
 		Game game = new Game();
-		assertEquals(game.giveOne(), 0);
+		assertEquals(game.giveOne(), 1);
 	}
 }
