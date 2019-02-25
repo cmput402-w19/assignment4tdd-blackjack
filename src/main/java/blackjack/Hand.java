5570aaf4293f93c1e45ce6a2e2a0c5bdac64247b
pennyfea <pennyfea@ualberta.ca>
2019-02-24 17:31:59 -0700

    GREEN & REFACTOR: Refactored Hand and HandTest to use enums

--- src/main/java/blackjack/Hand.java
+++ src/main/java/blackjack/Hand.java
@@ -12,9 +12,23 @@ public class Hand {
 	
 	public int getScore(){
 		int score = 0;
-		Collection<Card> collection = this.cards;
-		for(Card card : collection){
-			score += card.getScore();
+//		Collection<Card> collection = this.cards;
+		for(Card card : this.cards){
+			switch(card.getValue()) {
+			case ACE: score += 1; break;
+			case TWO: score += 2; break;
+			case THREE: score += 3; break;
+			case FOUR: score += 4; break;
+			case FIVE: score += 5; break;
+			case SIX: score += 6; break;
+			case SEVEN: score += 7; break;
+			case EIGHT: score += 8; break;
+			case NINE: score += 9; break;
+			case TEN: score += 10; break;
+			case JACK: score += 10; break;
+			case QUEEN: score += 10; break;
+			case KING: score += 10; break;
+			}
 		}
 		return score;
 	}
--- src/test/java/blackjack/HandTest.java
+++ src/test/java/blackjack/HandTest.java
@@ -7,26 +7,25 @@ import junit.framework.TestCase;
 public class HandTest extends TestCase {
 	
 	public void testGetScore(){
-		Card card1 = new Card("king","hearts");
+		Card card1 = new Card(Suit.HEART, Value.KING);
 		ArrayList<Card> cards1 = new ArrayList<Card>();
 		cards1.add(card1);
 		Hand hand1 = new Hand(cards1);
-		
 		assertEquals(hand1.getScore(), 10);
 		
-		Card card2 = new Card("king","hearts");
-		Card card3 = new Card("queen","hearts");
+		Card card2 = new Card(Suit.HEART, Value.KING);
+		Card card3 = new Card(Suit.HEART, Value.QUEEN);
 		ArrayList<Card> cards2 = new ArrayList<Card>();
 		cards2.add(card2);
 		cards2.add(card3);
 		Hand hand2 = new Hand(cards2);
-		
 		assertEquals(hand2.getScore(), 20);
 		
-		Card card4 = new Card("1","hearts");
-		Card card5 = new Card("2","hearts");
-		Card card6 = new Card("9","hearts");
-		Card card7 = new Card("10","hearts");
+		Card card4 = new Card(Suit.HEART, Value.ACE);
+		Card card5 = new Card(Suit.HEART, Value.TWO);
+		Card card6 = new Card(Suit.HEART, Value.NINE);
+		Card card7 = new Card(Suit.HEART, Value.TEN);
+		
 		ArrayList<Card> cards3 = new ArrayList<Card>();
 		cards3.add(card4);
 		cards3.add(card5);
@@ -39,15 +38,15 @@ public class HandTest extends TestCase {
 	}
 	
 	public void testGetCount(){
-		Card card1 = new Card("king","hearts");
+		Card card1 = new Card(Suit.HEART, Value.KING);
 		ArrayList<Card> cards1 = new ArrayList<Card>();
 		cards1.add(card1);
 		Hand hand1 = new Hand(cards1);
 		
 		assertEquals(hand1.getCount(), 1);
 		
-		Card card2 = new Card("king","hearts");
-		Card card3 = new Card("queen","hearts");
+		Card card2 = new Card(Suit.HEART, Value.KING);
+		Card card3 = new Card(Suit.HEART, Value.QUEEN);
 		ArrayList<Card> cards2 = new ArrayList<Card>();
 		cards2.add(card2);
 		cards2.add(card3);
@@ -55,10 +54,10 @@ public class HandTest extends TestCase {
 		
 		assertEquals(hand2.getCount(), 2);
 		
-		Card card4 = new Card("1","hearts");
-		Card card5 = new Card("2","hearts");
-		Card card6 = new Card("9","hearts");
-		Card card7 = new Card("10","hearts");
+		Card card4 = new Card(Suit.HEART, Value.ACE);
+		Card card5 = new Card(Suit.HEART, Value.TWO);
+		Card card6 = new Card(Suit.HEART, Value.NINE);
+		Card card7 = new Card(Suit.HEART, Value.TEN);
 		ArrayList<Card> cards3 = new ArrayList<Card>();
 		cards3.add(card4);
 		cards3.add(card5);
@@ -70,15 +69,15 @@ public class HandTest extends TestCase {
 	}
 	
 	public void	testGetCards(){
-		Card card1 = new Card("king","hearts");
+		Card card1 = new Card(Suit.HEART, Value.KING);
 		ArrayList<Card> cards1 = new ArrayList<Card>();
 		cards1.add(card1);
 		Hand hand1 = new Hand(cards1);
 		
 		assertEquals(hand1.getCards(), cards1);
 		
-		Card card2 = new Card("king","hearts");
-		Card card3 = new Card("queen","hearts");
+		Card card2 = new Card(Suit.HEART, Value.KING);
+		Card card3 = new Card(Suit.HEART, Value.QUEEN);
 		ArrayList<Card> cards2 = new ArrayList<Card>();
 		cards2.add(card2);
 		cards2.add(card3);
@@ -86,10 +85,10 @@ public class HandTest extends TestCase {
 		
 		assertEquals(hand2.getCards(), cards2);
 		
-		Card card4 = new Card("1","hearts");
-		Card card5 = new Card("2","hearts");
-		Card card6 = new Card("9","hearts");
-		Card card7 = new Card("10","hearts");
+		Card card4 = new Card(Suit.HEART, Value.ACE);
+		Card card5 = new Card(Suit.HEART, Value.TWO);
+		Card card6 = new Card(Suit.HEART, Value.NINE);
+		Card card7 = new Card(Suit.HEART, Value.TEN);
 		ArrayList<Card> cards3 = new ArrayList<Card>();
 		cards3.add(card4);
 		cards3.add(card5);
