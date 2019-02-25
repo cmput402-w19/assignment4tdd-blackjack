2fcefb24870ce3683624411571a32bb72baff04f
tyheise <tyler.heise@gmail.com>
2019-02-25 00:38:21 -0700

    Implemented skeleton methods and test  and constructor for Game. RED.

--- src/main/java/blackjack/Game.java
+++ src/main/java/blackjack/Game.java
@@ -1,7 +1,62 @@
 package blackjack;
 
+import java.util.ArrayList;
+
 public class Game {
-	public int giveOne(){
-		return 0;
+	
+	private Deck deck;
+	
+	private Player player1;
+	private Player player2;
+	
+	private Player winner;
+	
+	public void Game(){
+		deck.createDeck();
+		deck.shuffleDeck();
+		
+		ArrayList<ArrayList<Card>> cardsList = new ArrayList<ArrayList<Card>>();
+		cardsList = deck.dealCards();
+		
+		Hand hand1 = new Hand(cardsList.get(0));
+		Hand hand2 = new Hand(cardsList.get(1));
+		
+		player1 = new Player(hand1);
+		player2 = new Player(hand2);
 	}
+	
+	public Player getPlayer1(){
+		return null;
+	}
+
+	public Player getPlayer2(){
+		return null;
+	}
+
+	public Deck getDeck(){
+		return null;
+	}
+
+	public Player getWinner(){
+		return null;
+	}
+
+	// Sets environment up for the game
+	public void setUp(){
+		
+	}
+	
+	public void end(){
+		
+	}
+	
+	// Displays current state of the game
+	public void print(){
+		
+	}
+
+	public void play(){
+	
+	}
+
 }
--- src/test/java/blackjack/GameTest.java
+++ src/test/java/blackjack/GameTest.java
@@ -3,8 +3,27 @@ package blackjack;
 import junit.framework.TestCase;
 
 public class GameTest extends TestCase {
-	public void testGiveOne(){
-		Game game = new Game();
-		assertEquals(game.giveOne(), 0);
+	Game game;
+	
+	public void setUp(){
+		game = new Game();
 	}
+	
+	public void testGetPlayer1(){
+		
+	}
+	
+	public void testGetPlayer2(){
+		
+	}
+
+	public void testGetDeck(){
+		
+	}
+	
+	public void testGetWinner(){
+		
+	}
+	
 }
+
